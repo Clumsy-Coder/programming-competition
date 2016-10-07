dd4917f00fd1e5c00622ef048f6290d6eae92e8f
Clumsy-Coder <ubari94@gmail.com>
2016-10-07 14:30:58 -0600

    Create uva problem 10114.

---
+++ uva/c++/10114.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(){
+
+	int unsigned 	duration;
+	int unsigned 	depRecords;
+	double 			downPay;
+	double 			loanAmount;
+
+
+
+	cin >> duration >> downPay >> loanAmount >> depRecords;
+
+	cout << duration << downPay <<  loanAmount << depRecords << endl;
+
+
+
+	return 0;
+}
