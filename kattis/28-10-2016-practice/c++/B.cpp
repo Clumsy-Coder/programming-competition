d81e1f26a6c2951ebaeb7724dd9023c7d2ee0d09
Clumsy-Coder <ubari94@gmail.com>
2016-10-31 16:17:46 -0600

    Make some progress in completing problem B for 2016 Rocky Mountain Regional Practice. Not completed yet.

---
+++ kattis/28-10-2016-practice/c++/B.cpp
@@ -0,0 +1,71 @@
+//IN PROGRESS
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int offset;
+    string word;
+
+    while((cin >> offset >> word) && offset != 0)
+    {
+        if(offset == 0)
+        {
+            break;
+        }
+        reverse(word.begin(), word.end());
+        string tempstr;
+        bool isTrue = false;
+        for(int i = 0; i < word.size(); i++)
+        {
+            int temp = 0;
+            if(word[i] == '_')
+            {
+                temp = 91;
+
+            }
+            else if(word[i] == '.')
+            {
+                temp = 92;
+            }
+            else
+            {
+                temp = word[i];
+            }
+
+            if(temp + offset >= 91)
+            {
+                //if temp + offset == 91
+                if(temp + offset == 91)
+                {
+                    tempstr += '_';
+                }
+                //if temp + offset == 28
+                else if(temp + offset == 92)
+                {
+                    tempstr += '.';
+                }
+                else
+                {
+                    temp += offset;
+                    tempstr += ((char)temp % 28) + 56;
+                }
+
+            }
+
+            else
+            {
+                tempstr += (char)temp + offset;
+            }
+
+        }
+
+        cout << tempstr << endl;
+
+    }
+
+    return 0;
+}
