1148234db8d6954bf4c8e1af128ac32832a89227
Clumsy-Coder <ubari94@gmail.com>
2016-11-19 16:27:07 -0700

    Work on problem C in Rocky Mountain Regional Contest. Code outputs the correct answer from the provided test case. A certian test case is tripping it when it's submitted.

---
+++ kattis/29-10-2016-contest/c++/C.cpp
@@ -0,0 +1,38 @@
+//IN PROGRESS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    long long entries;
+
+    while(cin >> entries)
+    {
+        vector<long double> list;
+        for(int i = 0; i < entries; i++)
+        {
+            string password;
+            long double value;
+            cin >> password >> value;
+
+            list.push_back(value);
+        }
+
+        sort(list.begin(), list.end());
+        reverse(list.begin(), list.end());
+
+        long double answer = 0;
+        for(long long i = 0; i < list.size(); i++)
+        {
+            answer += list[i] * (i + 1);
+
+        }
+        cout << std::setprecision(5);
+        cout << answer << endl;
+    }
+    return 0;
+}
