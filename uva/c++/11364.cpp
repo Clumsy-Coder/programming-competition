55bac6f5fb7ca765ab0432bf7bf582931fc3dae3
Clumsy-Coder <ubari94@gmail.com>
2016-12-18 22:07:25 -0700

    Solve uva problem 11364

--- uva/c++/11364.cpp
+++ uva/c++/11364.cpp
@@ -1,13 +1,28 @@
-//IN PROGRESS
+//DONE
 #include <iostream>
 
-using std::cin;
-using std::cout;
-using std::endl;
+using namespace std;
 
 int main ()
 {
-	
+	int cases;
+	cin >> cases;
+	while(cases--)
+	{
+		int stops;
+		cin >> stops;
+		int r = -1000;
+		int l = 1000;
+
+		for(int i = 0; i < stops; i++)
+		{
+			int temp;
+			cin >> temp;
+			if(temp > r){r = temp;}
+			if(temp < l){l = temp;}
+		}
+		cout << 2 * (r - l) << endl;
+	}	
 
 	return 0;
 
