204b44434906ef8cf611ef9f49d282fbb0ec3807
Clumsy-Coder <ubari94@gmail.com>
2016-12-19 21:24:56 -0700

    Work on uva problem 10963

---
+++ uva/c++/10963.cpp
@@ -0,0 +1,38 @@
+//IN PROGRESS
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int cases = 0;
+    cin >> cases;
+
+    int first;
+    int second;
+
+    while(cases--)
+    {
+        int cols;
+        cin >> cols;
+        bool mergeable = true;
+
+        cin >> first >> second;
+        int d = first - second;
+
+        for(int i = 0; i < cols - 1; i++)
+        {
+            cin >> first >> second;
+            if(d != first - second)
+            {
+                mergeable = false;
+                break;
+            }
+        }
+
+        (mergeable) ? cout << "yes" << endl : cout << "no" << endl;
+        if(cases) cout << endl;
+    }
+
+    return 0;
+}
