82cfc7be23438818f2492f4215c027e1003acc0f
Clumsy-Coder <ubari94@gmail.com>
2017-01-17 22:01:11 -0700

    Solve and make uva problem 305 better
    This version uses math formula instead of vector.
    The answer is stored in int[14]
    
    It performs much faster

--- uva/c++/305.cpp
+++ uva/c++/305.cpp
@@ -1,115 +1,88 @@
 //DONE
 
 /*
-	get input 'm'
-	check if the answer is computed for the input 'm'
-		print the answer from map
-	else
-		call solve function
-			create a vector of char
-			add 'm' # of 'g' in the vector
-			add 'm' # of 'b' in the vector
-			create a copy vector of the original
-			while true //note: only break the loop when the answer is computed
-				compute the index to erase and set it to 'eraseIndex': (count + prevKill) % vector.size()
-					//note: prevKill and count is initially 0. stored in index form
-				set prevKill to eraseIndex
-				erase element at index eraseIndex
-				check if # of 'g' is NOT the same as 'm'	//check(vector<char>, char) returns the number of 'char' in the vector
-					count++
-					set the copy vector to the original
-					prevKill = 0
-					continue
+	init "m" array elements to -1
+	set m[0] = 2
+	while there's input and the input is NOT 0
+		print the value from solve function
+			solve function
+				if the input has already been calulated
+					return the answer
+				else
+					create variable
+						totalSize = 0	NOTE: this will either reduce OR be reset to 2 * k
+											NOTE: it will be reset IF a good guy was killed before a bad guy
+						prevKill = 0, 	NOTE: keeping track of the previous kill index
+											NOTE: gets reset to 0 IF a good guy was killed before a bad guy
+						killIndex = 0, 	NOTE: index that will be killed
+						count = 0		NOTE: this will be used as the answer
+					while true		NOTE: only breaks once an answer has been computed
+						set  killIndex (count + prevKill) % totalSize
+							it's starting by killing the first person.
+						set prevKill =  killIndex
+						if a good guy was killed before all the bad guys
+							increment count by 1
+							prevKill = 0;
+							totalSize = 2 * k
+							continue to the next iteration of the loop
+						end if
+						totalSize--		NOTE: this means a bad guy was killed without kill the good guy first
+						if the totalSize IS k AND the answer > k		NOTE: answer is found
+							update the array m[k - 1] to count + 1
+								NOTE: the answer is count + 1 because it was being computed in index form.
+							return the answer
+						end if
+					end while loop
 				end if
-
-				check if # of 'g' is 'm' AND # of 'b' is 0
-					//answer found. note: the answer is in index, so it's needs +1
-					add the answer to the map. mk[m] = count + 1
-					display the answer
-					break out of the loop
-				end if
-			end while
-		end solve function
-	end else
+			end solve function
+	end while loop
 */
 
 #include <iostream>
-#include <vector>
-#include <map>
 
 using namespace std;
 
-int check(const vector<char> &v, char value);
-void solve(int m, map<int, long long> &mk);
-
-map<int, long long> mk;
-
+int solve(int k);
+int m[14];
 int main()
 {
-	int value;
-	while(cin >> value && value)
+	for(int i = 0; i < 14; i++){m[i] = -1;}
+
+	m[0] = 2;
+	int curCase;
+	while(cin >> curCase && curCase)
 	{
-		if(mk.count(value) == 1)
-		{
-			cout << mk[value] << endl;
-		}
-		else
-		{
-			solve(value, mk);
-		}
+		cout << solve(curCase) << endl;
 	}
 }
 
-int check(const vector<char> &v, char value)
+int solve(int k)
 {
-	long long count = 0;
-	for(int i = 0; i < v.size(); i++)
+	if(m[k - 1] != -1)
 	{
-		if(v[i] == value)
-		{
-			count++;
-		}
+		return m[k - 1];
 	}
 
-	return count;
-}
-
-void solve(int m, map<int, long long> &mk)
-{
-	vector<char> k;
-	for(int i = 0; i < m; i++)
-	{
-		k.push_back('g');
-	}
-	for(int i = 0; i < m; i++)
-	{
-		k.push_back('b');
-	}
-	long long count = 0;
-	long long eraseIndex = 0;
+	int totalSize = 2 * k;
 	long long prevKill = 0;
-	vector<char> copy = k;
-
+	long long  killIndex = 0;
+	long long count = 0;
 	while(true)
 	{
-		eraseIndex = (count + prevKill) % copy.size();
-		prevKill = eraseIndex;
-
-		copy.erase(copy.begin() + eraseIndex);
-		if(check(copy, 'g') != m)
+		 killIndex = (count + prevKill) % totalSize;
+		prevKill =  killIndex;
+		if( killIndex < k)
 		{
 			count++;
-			copy = k;
 			prevKill = 0;
+			totalSize = 2 * k;
 			continue;
 		}
-
-		if(check(copy, 'g') == m &&
-		   check(copy, 'b') == 0)
+		totalSize--;
+		if(totalSize == k && count > k)
 		{
-			mk[m] = count + 1;
-			break;
+			m[k - 1] = count + 1;
+			return count + 1;
 		}
 	}
-	cout << count + 1 << endl;
 }
