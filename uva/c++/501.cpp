c966ac8da233e99c1d33409a1f13d6ddbfbe4c74
Clumsy-Coder <ubari94@gmail.com>
2017-01-30 11:52:33 -0700

    Solve uva problem 501
    Use vector to store the values.
    Use lower_bound to insert into the getQueue. This will make sure the vector is sorted O(log n + 1).
    Accessing the answer from getQueue will always be constant time O(1).
    
    Faster than using multiset or priority_queue
    - Multiset require an iterator to get the answer. O(n)
    - priority_queue require the element to be removed until the answer is retrieved,
    	and then insert the removed values again to retain integrity. This would take O(n * 2)
    Both multiset and priority_queue are too slow to pass Uva Judge

--- uva/c++/501.cpp
+++ uva/c++/501.cpp
@@ -1,20 +1,28 @@
+//DONE
+/*
+    use vector for M values and to compute the answer, because accessing them is constant time.
+        Getting the answer from getQueue is always constant, compared to
+        multiset, or priority queue.
+        Multiset and priority queue require O(n) to get the value that is between index 0 and size - 1
+            That would take too long to pass the Uva judge.
+    When inserting into getQueue, use lower_bound to find the position to be
+    inserted in. This will end up causing the vector to sorted.
+*/
+
 #include <iostream>
 #include <vector>
-#include <set>
 
 using namespace std;
 
 int main()
 {
-    int cases;
-    cin >> cases;
+    int cases; cin >> cases;
     while(cases--)
     {
         cin.ignore();
-        int numAdd, numGet;
-        cin >> numAdd >> numGet;
+        int numAdd, numGet; cin >> numAdd >> numGet;
         vector<int> addQueue(numAdd);
-        multiset<int> getQueue;
+        vector<int> getQueue;
         for(int i = 0; i < numAdd; i++)
         {
             cin >> addQueue[i];
@@ -22,21 +30,20 @@ int main()
 
         for(int i = 0, curAdd = 0; i < numGet; i++)
         {
-            int temp;
-            cin >> temp;
-
+            int temp; cin >> temp;
             //add to getQueue if not enough elements
-            while(getQueue.size() < temp && getQueue.size() - temp)
+            while(getQueue.size() < temp)
             {
-                getQueue.insert(addQueue[curAdd++]);
-            }
+                //use lower bound because it returns the index of the value closest to it.
+                //  The first occurance of the value
+                //the iterator value return is in 0 based.
+                //this is self sorting when inserting to the vector
+                //lower bound will always run on log n + 1 because it's self sorting.
+                auto iterator = lower_bound(getQueue.begin(), getQueue.end(), addQueue[curAdd]);
+                getQueue.insert(iterator, addQueue[curAdd++]);
 
-            auto iterator = getQueue.begin();
-            for(int j = 0; j < i; j++)
-            {
-                iterator++;
             }
-            cout << *iterator << endl;
+            cout << getQueue[i] << endl;
         }
         if(cases){cout << endl;}
     }
