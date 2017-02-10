734c5038296b871cec1b94395fdb5deb14475541
Clumsy-Coder <ubari94@gmail.com>
2017-02-09 19:05:09 -0700

    Add some comments for uva problem 11195
    Explain what the bitset is used for and
    what the recursion was doing
    
    Code was from Competitive Programming 3 pg 74 - 76

--- uva/c++/11195.cpp
+++ uva/c++/11195.cpp
@@ -14,7 +14,7 @@ int n = 0;
 int answer = 0;
 char board[14][14];
 void solve(int col);
-bitset<30> row, leftDiagonal, rightDiagonal;
+bitset<15> row, leftDiagonal, rightDiagonal;
 int main()
 {
     int cases = 0;
@@ -48,18 +48,28 @@ void solve(int col)
     {
         for(int curRow = 0; curRow < n; curRow++)
         {
-            if(board[curRow][col] != '*' &&
-               !row[curRow] &&
-               !leftDiagonal[curRow - col + n - 1] &&
-               !rightDiagonal[curRow + col])
+            //board[curRow][col] != '*'     : ignore the bad spots
+            //!row[curRow]                  : if there's no queen on this row
+            //!leftDiagonal[curRow - col]   : if there's no queen on the left slope. Using the idea of similar triangles
+            //!rightDiagonal[curRow + col]  : if there's no queen on the right slope. Using the idea of similar triangles
+
+            //similar triangles idea: 
+            //imagine the board and the queen in a coordinate system, you can use a slope to determine if the current queen
+            //and the next queen in the diagonal (left or right) are getting in the way (same path) if they have the same slope.
+            //you don't need to calculate the hypotenuse, using the x and y values works.
+            //
+            //since it's recursing, the bitset is keeping track of the previous values and can be used to do a check, 
+            //making the checks more restrictive since the chance of placing a queen is reduced.
+            //
+            //after checking all the possible combinations in that current state, reset the value that was recently 
+            //modified so the next iteration can use it.
+            if(board[curRow][col] != '*' && !row[curRow] && !leftDiagonal[curRow - col] && !rightDiagonal[curRow + col])
             {
-                row[curRow] = true;
-                leftDiagonal[curRow - col + n - 1] = true;
-                rightDiagonal[curRow + col] = true;
+                //add a queen at the position and recurse with that information for the next queen
+                row[curRow] = leftDiagonal[curRow - col] = rightDiagonal[curRow + col] = true;
                 solve(col + 1);
-                row[curRow] = false;
-                leftDiagonal[curRow - col + n - 1] = false;
-                rightDiagonal[curRow + col] = false;
+                //remove the queen at the position so the queen in the next row can be evaluated.
+                row[curRow] = leftDiagonal[curRow - col] = rightDiagonal[curRow + col] = false;
             }
         }
     }
