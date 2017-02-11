36abf92c2806db2eb5971c1e26880116d35e1758
Clumsy-Coder <ubari94@gmail.com>
2017-02-10 22:28:33 -0700

    Solve uva problem 10026
    Use a priority queue with a custom
    comparision operator to define how the
    elements are to be sorted.

--- uva/c++/10026.cpp
+++ uva/c++/10026.cpp
@@ -1,3 +1,4 @@
+//DONE
 #include <iostream>
 #include <queue>
 
@@ -8,27 +9,17 @@ struct job
     int id;
     int time;
     int fine;
-};
-
-class ComparisionClass
-{
-    public:
-        bool operator() (job &job1, job &job2)
+    bool operator() (const job &job1, const job &job2)
+    {
+        if((job1.time * job2.fine) == (job2.time * job1.fine))
         {
-            if((job1.time * job2.fine) == (job2.time * job1.fine) ||
-               (job1.time * job2.fine) < (job2.time * job1.fine))
-            {
-                return false;
-            }
-            else if((job1.time * job2.fine) < (job2.time * job1.fine))
-            {
-                return true;
-            }
-            return (job1.time * job2.fine) != (job2.time * job1.fine);
+            return job2.id < job1.id;
         }
+        return (job1.time * job2.fine) > (job2.time * job1.fine);
+    };
 };
 
-priority_queue<job, vector<job>, ComparisionClass> allJobs;
+priority_queue<job, vector<job>, job> allJobs;
 
 int main()
 {
@@ -36,18 +27,14 @@ int main()
     cin >> cases;
     while(cases--)
     {
-        // allJobs.clear();
         int numJobs;
         cin >> numJobs;
         for(int i = 0; i < numJobs; i++)
         {
             job curJob; curJob.id = i + 1;
             cin >> curJob.time >> curJob.fine;
-            // cout << curJob.time << " " << curJob.fine << endl;
             allJobs.push(curJob);
         }
-
-        // cout << allJobs.top().id << " "; allJobs.pop();
         while(!allJobs.empty())
         {
             cout << allJobs.top().id;
