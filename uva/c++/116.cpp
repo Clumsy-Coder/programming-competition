b6f22913af81e5e3c3cb2dc5c096355e10ba3abe
Clumsy-Coder <ubari94@gmail.com>
2017-03-02 11:58:50 -0700

    Work on uva problem 116

---
+++ uva/c++/116.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int row, col;
+const int MAX_ROW = 10;
+const int MAX_COL = 100;
+int matrix [MAX_ROW][MAX_COL];
+int dist [MAX_ROW][MAX_COL];
+void generateDist();
+void backtrack();
+
+void print()
+{
+    for(int curR = 0; curR < row; curR++)
+    {
+        for(int curC = 0; curC < col; curC++)
+        {
+            cout << dist[curR][curC] << "\t";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    while(cin >> row >> col)
+    {
+        for(int curR = 0; curR < row; curR++)
+        {
+            for(int curC = 0; curC < col; curC++)
+            {
+                cin >> matrix[curR][curC];
+                if(curC == col - 1)
+                {
+                    dist[curR][curC] = matrix[curR][curC];
+                }
+            }
+        }
+
+        generateDist();
+        // backtrack();
+    }
+}
+
+void generateDist()
+{
+    for(int curC = col - 1; curC > 0; curC--)
+    {
+        for(int curR = 0; curR < row; curR++)
+        {
+            //left
+            int left = matrix[curR][curC - 1];
+            //up. if curR = 0, then wrap around to the bottom. curR = row - 1
+            int up = (curR == 0) ? matrix[row - 1)][curC - 1] : matrix[(curR - 1) % row][curC - 1];
+            //down. if curR = row - 1, then wrap around to the top, curR = 0
+            int down = (curR == row - 1) ? matrix[0][curC - 1] : matrix[(curR + 1) % row][curC - 1];
+
+            dist[curR][curC - 1] = matrix[curR][curC] + min({left, up, down});
+        }
+    }
+
+    print();
+}
+
+void backtrack()
+{
+    // vector<int> path;
+    // for(int curC = 0; curC < col; curC++)
+    // {
+    //     int curMin = 9000;
+    //     for(int curR = 0; curR < row; curR++)
+    //     {
+    //         if(matrix[curR][curC] < curMin)
+    //         {
+    //             curMin = curR;
+    //             path.push_back(curR);
+    //         }
+    //     }
+    // }
+}
