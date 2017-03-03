b8d2515afe5d45123d091e1237d7101c2a8e78a3
Clumsy-Coder <ubari94@gmail.com>
2017-03-02 19:34:56 -0700

    Add some comments for uva problem 116

--- uva/c++/116.cpp
+++ uva/c++/116.cpp
@@ -1,3 +1,14 @@
+//DONE
+/*
+	Generate the distance from right to left for all elements in the matrix
+		distance at col - 1 += min (right, up down)
+			right: same row, one column to the right
+			up: one row up and column to the right
+			down: one row down and one column to the right
+	Find the best path (with minimum distance)
+	Print the answer
+*/
+
 #include <iostream>
 #include <algorithm>
 #include <utility>
@@ -5,24 +16,11 @@
 using namespace std;
 
 int row, col;
-const int MAX_ROW = 10;
-const int MAX_COL = 100;
+const int MAX_ROW = 11;
+const int MAX_COL = 101;
 int matrix [MAX_ROW][MAX_COL];
 void generateDist();
 void backtrack();
-
-void print()
-{
-    for(int curR = 0; curR < row; curR++)
-    {
-        for(int curC = 0; curC < col; curC++)
-        {
-            cout << matrix[curR][curC] << "\t";
-        }
-        cout << endl;
-    }
-    cout << endl;
-}
 
 int main()
 {
@@ -55,25 +53,21 @@ void generateDist()
             //down. if curR = row - 1, then wrap around to the top, curR = 0
             int down = matrix[(curR + 1) % row][curC + 1];
 
-            matrix[curR][curC] = matrix[curR][curC] + min({left, up, down});
+            matrix[curR][curC] += min({left, up, down});
         }
     }
-
-    // print();
 }
 
 void backtrack()
 {
-    // vector<int> path;
-    // int curLoc [] = {0, 0};
     int best = matrix[0][0], index = 0, distance;
     for(int i = 0; i < row; i++)
     {
     	if(matrix[i][0] < best)
-			{
-				index = i;
-				best = matrix[i][0];
-			}
+		{
+			index = i;
+			best = matrix[i][0];
+		}
     }
 
     distance = matrix[index][0];
@@ -86,9 +80,7 @@ void backtrack()
     		cout << " ";
     	}
     	int right = matrix[index] [i + 1];
-		//up. if curR = 0, then wrap around to the bottom. curR = row - 1
 		int up = matrix[(index + row - 1) % row][i + 1];
-		//down. if curR = row - 1, then wrap around to the top, curR = 0
 		int down = matrix[(index + 1) % row][i + 1];
 
 		auto temp = min({pair<int, int>(right, index), 
@@ -100,75 +92,4 @@ void backtrack()
 
     cout << endl;
     cout << distance << endl;
-
-    return;
-
-
-
-   //  for(int curC = 0; curC < col; )
-   //  {
-   //      for(int curR = 0; curR < row; curR++)
-   //      {
-   //          if(curC == 0)
-			// {
-   //              if(matrix[curR][curC] < matrix[curLoc[0]][curLoc[1]])
-			// 	{
-			// 		curLoc[0] = curR;
-			// 		curLoc[1] = curC;
-			// 	}
-			// }
-   //          else
-			// {
-			// 	int right = matrix[curLoc[0]] [curLoc[1] + 1];
-			// 	//up. if curR = 0, then wrap around to the bottom. curR = row - 1
-			// 	int up = matrix[(curLoc[0] + row - 1) % row][curLoc[1] + 1];
-			// 	//down. if curR = row - 1, then wrap around to the top, curR = 0
-			// 	int down = matrix[(curLoc[0] + 1) % row][curLoc[1] + 1];
-
-			// 	auto temp = min({pair<int, int>(right, curLoc[0]), 
-			// 					 pair<int, int>(up, (curLoc[0] + row - 1) % row), 
-			// 					 pair<int, int>(down, (curLoc[0] + 1) % row)});
-
-			// 	cout << "right: " << right << endl;
-			// 	cout << "up: " << up << endl;
-			// 	cout << "down: " << down << endl;
-			// 	cout << "temp: " << temp.first << endl;
-
-			// 	//temp.second == curLoc[0] used for tie breaker
-			// 	if(temp.first == right && temp.second == curLoc[0])
-			// 	{
-			// 		curLoc[1] = curLoc[1] + 1;
-			// 		cout << "right\n\n";
-			// 	}
-			// 	else if(temp.first == up)
-			// 	{
-			// 		curLoc[0] = (curLoc[0] + row - 1) % row;
-			// 		curLoc[1] = curLoc[1] + 1;
-			// 		cout << "up\n\n";
-			// 	}
-			// 	else if(temp.first == down)
-			// 	{
-			// 		curLoc[0] = (curLoc[0] + 1) % row;
-			// 		curLoc[1] = curLoc[1] + 1;
-			// 		cout << "down\n\n";
-			// 	}
-			// 	curC = curLoc[1];
-			// 	path.push_back(curLoc[0]);
-			// }
-
-			// if(curR == row - 1 && curC == 0)
-			// {
-			// 	path.push_back(curLoc[0]);
-			// 	curC = curLoc[1] + 1;
-			// }
-   //      }
-        
-   //  }
-
-   //  for(unsigned int i = 0; i < path.size(); i++)
-   //  {
-   //      cout << path[i] + 1 << " ";
-   //  }
-   //  cout << endl << matrix[path[0]][0] << endl;
-   //  // cout << "----------\n\n";
 }
