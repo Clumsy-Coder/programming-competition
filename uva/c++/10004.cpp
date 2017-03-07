0c028c56caebcad348c69aaccb30432bf531d12d
Clumsy-Coder <ubari94@gmail.com>
2017-03-07 13:12:16 -0700

    Solve uva problem 10004
    Use DFS traversal and mark each node a colour.
    go through all the nodes
    	for each node's neighbour
    		check if the colour is NOT set.
    			set the colour (different from parent)
    			add to the  stack
    		check if the colour is the same as the parent's colour
    			it's not bicolourable
    
    This approach has bipartite elements

--- uva/c++/10004.cpp
+++ uva/c++/10004.cpp
@@ -1,31 +1,37 @@
+//DONE
+/*
+    Use DFS traversal approach. Same idea as bipartite
+    
+    Add the node (parent node) into stack and set the colour.
+    while there's elements in the stack
+        get the node (parent node) in the stack and pop it.
+        for each of the parents's node neighbours,
+            check if the colour is NOT set.
+                set the colour (alternate from the parent) and to the stack
+            check if the colour is the same as the parent.
+                then it's not bicolourable
+*/
+
 #include <iostream>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
+const int NOT_SET = -1;
+const int BLACK = 0;
+const int RED = 1;
+
+//used for constructing the adjacency list
 struct node
 {
     int nodeNum;
-    int colour = -1;     //-1 = not set, 0 = black colour, 1 = red colour
+    int colour = NOT_SET;     //-1 = not set, 0 = black colour, 1 = red colour
     vector<int> neighbour;
 };
 
 void readInput(vector<node> &adjList);
 void solve(vector<node> &adjList);
-void print(const vector<node> &adjList);
-
-void print(vector<node> &adjList)
-{
-    for(int i = 0; i < adjList.size(); i++)
-    {
-        cout << i << " ";
-        for(int neighbourNode = 0; neighbourNode < adjList[i].neighbour.size(); neighbourNode++)
-        {
-            cout << adjList[i].neighbour[neighbourNode] << " ";
-        }
-        cout << endl;
-    }
-}
 
 int main()
 {
@@ -34,8 +40,7 @@ int main()
     {
         vector <node> adjacencyList(numNodes);
         readInput(adjacencyList);
-        print(adjacencyList);
-        cout << endl;
+        solve(adjacencyList);
     }
 }
 
@@ -50,4 +55,34 @@ void readInput(vector<node> &adjList)
         adjList[node].neighbour.push_back(neighbourNode);
         adjList[neighbourNode].neighbour.push_back(node);
     }
+}
+
+void solve(vector<node> &adjList)
+{
+    stack <node> dfs;
+    adjList[0].colour = BLACK;
+    dfs.push(adjList[0]);
+    bool bicolourable = true;
+    while(!dfs.empty() && bicolourable)
+    {
+        node parentNode = dfs.top();
+        dfs.pop();
+        //go through all it's neighbours and check
+        for(int i = 0; i < parentNode.neighbour.size(); i++)
+        {
+            node *curNode = &adjList[parentNode.neighbour[i]];
+            if(curNode->colour == NOT_SET)
+            {
+                curNode->colour = (parentNode.colour == BLACK) ? RED : BLACK;
+                dfs.push(*curNode);
+            }
+            else if(curNode->colour == parentNode.colour)
+            {
+                bicolourable = false;
+                break;
+            }
+        }
+    }
+
+    (bicolourable) ? cout << "BICOLORABLE.\n" : cout << "NOT BICOLORABLE.\n";
 }
