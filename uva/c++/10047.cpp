a9758cce5702e4e7a6ffc5e7cff510322eb6b09f
Clumsy-Coder <ubari94@gmail.com>
2017-03-09 21:26:53 -0700

    Work on uva problem 10047

--- uva/c++/10047.cpp
+++ uva/c++/10047.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <queue>
+#include <string.h>
 
 using namespace std;
 
-const int dirX [] = {0 , 1, 0, -1};
-const int dirY [] = {-1, 0, 1, 0 };
+const int dirX [] = {-1, 0, 1, 0 };
+const int dirY [] = {0 , 1, 0, -1};
+const int RIGHT = 1;
+const int LEFT = 3;
+bool status [30][30][5][5]; //[x][y][dir][colour]
 
 void readInput();
 
@@ -18,7 +22,7 @@ struct node
     int dir; //0 = north, 1 = east, 2 = south, 3 = west
 
     node() {}
-    node(int curX, int curY, int curColour, int curTime, int curDir)
+    node(int curX, int curY, int curColour=0, int curTime=0, int curDir=0)
     {
         x = curX; y = curY;
         colour = curColour;
@@ -26,48 +30,58 @@ struct node
         dir = curDir;
     }
 
-    node turn(int direction)
+    node turn(int direction)   //direction 1 = right, direction 3 = left
     {
         return node( x, y,                           //x and y
-                     (dir + 1 + direction * 2) % 4,  //direction
-                     colour,                              //colour
-                     time + 1);                         //time
+                     (dir + direction) % 4,          //direction
+                     colour,                         //colour
+                     time + 1);                      //time
     }
 
     node moveForward()
     {
-        return node( x + dirY[dir], y + dirX[dir],  //x and y
-                     dir,                       //direction
-                     (colour + 1) % 5,          //colour
-                     time + 1);                 //time
+        return node( x + dirX[dir], y + dirY[dir],  //x and y
+                     dir,                           //direction
+                     (colour + 1) % 5,              //colour
+                     time + 1);                     //time
+    }
+
+    bool valid()
+    {
+        return (x + dirX[dir] < numRow &&  x + dirX[dir] >= 0) && 
+               (y + dirY[dir] < numCol && y + dirY[dir] >= 0);
+
     }
 
 };
 
-int status [30][30][5][5]; //[x][y][dir][colour]
-char matrix [30][30];
+
+char matrix [27][27];
 node start, finish;
+int cases = 1;
 
 void print();
 void solve();
 
 int main()
 {
-    int cases = 1;
+    
     while(cin >> numRow >> numCol && (numRow && numCol))
     {
-        readInput();
-        // print();
-        cout << "Case #" << cases++ << endl;
         if(cases > 1)cout << endl;
+        readInput();
+        solve();
+        // cout << "Case #" << cases++ << endl;
     }
 }
 
 void readInput()
 {
-    for(int i = 0; i < numRow; i++)
+    memset(status, false, sizeof(status));
+    memset(matrix, '#', sizeof(matrix));
+    for(int i = 1; i < numRow + 1; i++)
     {
-        for(int j = 0; j < numCol; j++)
+        for(int j = 1; j < numCol + 1; j++)
         {
             cin >> matrix[i][j];
             if(matrix[i][j] == 'S')
@@ -86,21 +100,6 @@ void readInput()
     }
 }
 
-void print()
-{
-    for(int i = 0; i < numRow; i++)
-    {
-        for(int j = 0; j < numCol; j++)
-        {
-            cout << matrix[i][j];
-        }
-        cout << endl;
-    }
-
-    cout << "start x: " << start.x << "\t" << "start y: " << start.y << endl;
-    cout << "finish x: " << finish.x << "\t" << "finish y: " << finish.y << endl;
-}
-
 void solve()
 {
     queue <node> bfs;
@@ -117,25 +116,32 @@ void solve()
             answer = parent;
             break;
         }
-        //ignore if current grid is #
-        if(matrix[parent.x][parent.y] == '#') {continue;}
-        //ignore if already visited
-        if(status[parent.x][parent.y][parent.dir][parent.colour]) {continue;}
-        //mark current coordinate as visited
-        status[parent.x][parent.y][parent.dir][parent.colour] = 1;
-        if (!(
-                (parent.x + dirY[parent.dir] < 1) ||         //
-                (parent.x + dirY[parent.dir] > numRow) ||    //if
-                (parent.y + dirX[parent.dir] < 1) ||
-                (parent.y + dirX[parent.dir] > numCol)))
+        // //ignore if current grid location is #
+        // if(matrix[parent.x][parent.y] == '#') {continue;}
+        // //ignore if already visited
+        // if(status[parent.x][parent.y][parent.dir][parent.colour]) {continue;}
+        // //if it's in bounds
+        // if (!parent.valid()){continue;}
+
+        if( matrix[parent.x][parent.y] == '#' || 
+            status[parent.x][parent.y][parent.dir][parent.colour] ||
+            !parent.valid())
         {
             continue;
         }
 
+        //mark current coordinate as visited
+        status[parent.x][parent.y][parent.dir][parent.colour] = true;
+        //turn right
+        bfs.push(parent.turn(RIGHT));
+        //turn left
+        bfs.push(parent.turn(LEFT));
+        //move foreward
+        bfs.push(parent.moveForward());
 
     }
 
+    cout << "Case #" << cases++ << endl;
     (answer.time != 0) ? cout << "minimum time = " << answer.time << " sec\n" :
                        cout << "destination not reachable\n";
-
 }
