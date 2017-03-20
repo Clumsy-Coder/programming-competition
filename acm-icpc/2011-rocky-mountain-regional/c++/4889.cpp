7fbfc2990639c21d47c7d520a79642875d3863af
Clumsy-Coder <ubari94@gmail.com>
2017-03-20 11:27:43 -0600

    Work on acm-icpc problem 4889

--- acm-icpc/2011-rocky-mountain-regional/c++/4889.cpp
+++ acm-icpc/2011-rocky-mountain-regional/c++/4889.cpp
@@ -5,22 +5,26 @@ using namespace std;
 int main()
 {
 	double length, width, thickness;
-	while(cin >> length >> width >> thickness)
+	while(cin >> length >> width >> thickness && (length && width && thickness))
 	{
+		cout << length << " " << width << " " << thickness << endl;
 		//letter
 		if( (length >= 125 || length <= 290) &&
-			(width >= 90 || width <= 155) && 
+			(width >= 90 || width <= 155) &&
 			(thickness >= 0.25 || thickness <= 7.0))
 		{
 			cout << "letter" << endl;
 		}
 		//packet
-		else if()
+		else if( (length >= 290 || length <= 380) &&
+				 (width >= 155 || width <= 300) &&
+			 	 (thickness >= 7.0 || thickness <= 50))
 		{
 			cout << "packet" << endl;
 		}
 		//parcel
-		else if()
+		else if( (length >= 380 || length <= 2100) &&
+				 (thickness * 2) + (width * 2) <= 2100)
 		{
 			cout << "parcel" << endl;
 		}
