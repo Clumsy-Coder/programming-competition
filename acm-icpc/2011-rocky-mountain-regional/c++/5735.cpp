52673ec313c30cf248db1a4c970fedb0a4963029
Clumsy-Coder <ubari94@gmail.com>
2017-03-24 16:55:18 -0600

    Work on acm-icpc 2011-rocky mountain problem 5735

--- acm-icpc/2011-rocky-mountain-regional/c++/5735.cpp
+++ acm-icpc/2011-rocky-mountain-regional/c++/5735.cpp
@@ -1,43 +1,81 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <utility>
+#include <queue>
 
 using namespace std;
 
+void readInput();
+void solve();
+
+vector<pair<int, int>> stocks;
+unsigned long long n, k1, k2, cases = 1;
+
 int main()
 {
-	int n, k1, k2;
-	int cases = 1;
 	while(cin >> n >> k1 >> k2 && (n && k1 && k2))
 	{
-		vector <int> prices;
-		
-		bool donek1 = false;
-		for(int i = 0; i < n; i++)
-		{
-			int value;
-			cin >> value;
-			prices.push_back(value);
-		}
-		
 		cout << "Case " << cases++ << endl;
-		sort(prices.begin(), prices.end());
-
-		// for(auto val : prices)
-		// 	cout << val << " ";
-
-		// cout << endl;
-
-		for(int i = 0; i < k1; i++)
-		{
-			cout << i + 1 << " ";
-		}
-		cout << endl;
-
-		for(int i = prices.size(); i > prices.size() - k2; i--)
-		{
-			cout << i<< " ";
-		}
-		cout << endl;
+		readInput();
+		solve();
 	}
+}
+
+void readInput()
+{
+	stocks.clear();
+	for(unsigned long long i = 0; i < n; i++)
+	{
+		long long price; cin >> price;
+		pair<int, int> stockPrice(price, i + 1);	//stock price, date (in this case a counter)
+		stocks.push_back(stockPrice);
+	}
+}
+
+void solve()
+{
+	//sort by price. ascending
+	std::sort(stocks.begin(), stocks.end(), [](const std::pair<int,int> &left, const std::pair<int,int> &right) {
+		return left.first < right.first;
+	});
+
+	vector<pair<int, int>> k1V;
+	for(int i = 0; i < k1; i++)
+	{
+		// cout << stocks[i].second << " ";
+		k1V.push_back(stocks[i]);
+	}
+	// cout << endl;
+
+	//sort by price. deascending
+	vector<pair<int, int>> k2V;
+	std::sort(stocks.begin(), stocks.end(), [](const std::pair<int,int> &left, const std::pair<int,int> &right) {
+		return left.first > right.first;
+	});
+
+	for(int i = 0; i < k2; i++)
+	{
+		// cout << stocks[i].second << " ";
+		k2V.push_back(stocks[i]);
+	}
+
+	//sort by time. ascending
+	std::sort(k1V.begin(), k1V.end(), [](const std::pair<int,int> &left, const std::pair<int,int> &right) {
+		return left.second < right.second;
+	});
+
+	// for(auto i : k1V)
+	for(int i = 0; i < k1V.size(); i++)
+		cout << k1V[i].second << (i == k1V.size() - 1 ? "" : " ");
+	cout << endl;
+
+	//sort by time. deascending
+	std::sort(k2V.begin(), k2V.end(), [](const std::pair<int,int> &left, const std::pair<int,int> &right) {
+		return left.second > right.second;
+	});
+
+	for(int i = 0; i < k2V.size(); i++)
+		cout << k2V[i].second << (i == k2V.size() - 1 ? "" : " ");
+	cout << endl;
 }
