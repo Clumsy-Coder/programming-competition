aea92ad5d841e8477420a34836345acafef9e8f3
Clumsy-Coder <ubari94@gmail.com>
2017-03-30 19:32:37 -0600

    Solve uva problem 384

--- uva/c++/384.cpp
+++ uva/c++/384.cpp
@@ -1,10 +1,16 @@
-//IN PROGRESS
+//DONE
+/*
+	Make a function for the two rules slump and slimp
+	Use recursion to solve the problem
+*/
+
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-bool isSlump(int start, int end);
-bool isSlimp(int start, int end);
+bool isSlump(string str);
+bool isSlimp(string str);
 bool isSlurpy();
 
 string text;
@@ -22,25 +28,69 @@ int main()
 
 bool isSlurpy()
 {
-    bool answer = false;
     cin >> text;
 
-    for(int i = 0; i < text.size(); i++)
+    for(int i = text.size(); i > 0; i--)
     {
-        answer = isSlump(i + 1, text.size() - 1) && isSlimp(0, i);
-
-        if(answer) return answer;
+    	if(text[i] == 'H' || text[i] == 'C'){
+	    	string str1 = text.substr(0,i+1);
+	    	string str2 = text.substr(i+1, text.size()-i);
+	        if(isSlimp(str1) && isSlump(str2)){return true;}
+	    }
     }
 
-    return answer;
+    return false;
 }
 
-bool isSlump(int start, int end)
+bool isSlump(string str)
 {
+	if(str[0] != 'D' && str[0] != 'E') {return false;}
+	if(str[1] != 'F') {return false;}
+	
+	int i = 2;
+	while(str[i] == 'F'){i++;}
+	
+	if(i == str.size() - 1)
+	{
+		if(str[i] == 'G')
+			return true;
+	}
+	else
+	{
+		if(isSlump(str.substr(i, str.size() - i)))
+		{
+			return true;
+		}
+	}
 
+	return false;
 }
 
-bool isSlimp(int start, int end)
+bool isSlimp(string str)
 {
+	if(str[0] != 'A')
+	{
+		return false;
+	}
 
+	if(str.size() == 2)
+	{
+		if(str[1] == 'H')
+		{
+			return true;
+		}
+	}
+
+	else
+	{
+		if(str[1] == 'B' && isSlimp(str.substr(2, str.size() - 3)) && str[str.size() - 1] == 'C')
+		{
+			return true;
+		}
+		else if(isSlump(str.substr(1, str.size() - 2)) && str[str.size() - 1] == 'C')
+		{
+			return true;
+		}
+	}
+	return false;
 }
